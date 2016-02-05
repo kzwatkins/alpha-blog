0c1989d7272e17db6fb39bb47b1e2eb0bd8735d7
K. Z. Watkins <kzw@acm.org>
2016-02-05 15:13:17 -0500

    Fixed create with error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,13 +8,23 @@ class ArticlesController < ApplicationController
 	
 	end
 	
+	def show
+		@article = Article.find(params[:id])
+	end
+	
 	def create
 		# render plain: params[:article].inspect
 		@article = Article.new(article_params)
-		@article.save	
-		redirect_to_articles_show(@article)
+		
+		if	(@article.save)
+			flash[:notice] = "Article was successfully created."
+			redirect_to article_path(@article)
+		else
+			# flash[:notice] = "Could not create article."
+			render :new
+		end
 	end
-	
+
 	private
 		def article_params # whitelist the parameters
 			params.require(:article).permit(:title, :description)
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ root 'pages#home'
   #   get 'products/:id' => 'catalog#view'
   
     get 'pages/about' => 'pages#about'
+
+	resources :articles
+	
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
