7a96ba566a98af39f5d883f68f63adf2e2183dd1
K. Z. Watkins <kzw@acm.org>
2016-02-26 12:31:51 -0500

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
+	validates :username, 
+			presence: true, uniqueness: {case_sensitive: false},
+			length: {minimum: 3, maximum: 25}
+	
+	VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]{1,50}\.[a-z]{2,10}\z/i
+	
+	validates :email,
+			presence: true, uniqueness: {case_sensitive: false},
+			length: {minimum: 3, maximum: 50},
+			format: {with: VALID_EMAIL_REGEX}
 
 end
